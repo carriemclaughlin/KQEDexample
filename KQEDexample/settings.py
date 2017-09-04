07b368473abf48ec1d64083e91448b75f4232569
carriemclaughlin <camclaug@gmail.com>
2017-09-04 01:56:30 -0400

    settings for carrielicious

--- KQEDexample/settings.py
+++ KQEDexample/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'oxpcr7hklz*l6xv&mhiwf8-=6w6z^=rbg$5&*2r=zygvtivq)e'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['example.carrielicious.com']
 
 
 # Application definition
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
